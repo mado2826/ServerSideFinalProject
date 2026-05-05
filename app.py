c18ececa8a425eb0f6a0b508826ae7783a703e37
mado2826 <kitkat8199@gmail.com>
2026-05-05 19:54:30 -0400

    server template

--- app.py
+++ app.py
@@ -1,2 +1,45 @@
-import flask
+from flask import Flask, jsonify, Response, request, send_file, redirect, jsonify, render_template, make_response, flash
+
+app = Flask(__name__)
+app.secret_key = 'skibiditoilet6767'
+
+@app.route('/')
+def start():
+    pass
+
+@app.route('/login')
+def login():
+    pass
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    pass
+
+@app.route('/map')
+def map():
+    pass
+
+@app.route('/review', methods = ['POST'])
+def create_review():
+    pass
+
+@app.route('/review', methods = ['PUT'])
+def update_review():
+    pass
+
+@app.route('/review', methods = ['DELETE'])
+def delete_review():
+    pass
+
+@app.route('/event', methods = ['GET','POST'])
+def create_event():
+    pass
+
+@app.route('/event', methods = ['PUT'])
+def update_event():
+    pass
+
+@app.route('/event', methods = ['DELETE'])
+def delete_event():
+    pass
 
